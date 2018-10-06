b62cf13e2fc4824fa847ca0231111ed71f7b6d5f
Balazs Demeter <balazsdemeter92@gmail.com>
2018-10-06 19:00:35 +0200

    fixed run.png

--- b/CMakeFiles/GDE.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/GDE"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/GDE.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
